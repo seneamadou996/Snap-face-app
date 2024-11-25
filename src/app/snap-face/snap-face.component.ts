ced8c80654fd37e1354962acd303f20b8d3d9a62
Amadou Sene <amadousene64@gmail.com>
2024-11-25 22:48:35 +0000

    Reaction au evenement du template

--- src/app/snap-face/snap-face.component.ts
+++ src/app/snap-face/snap-face.component.ts
@@ -14,13 +14,25 @@ export class SnapFaceComponent implements OnInit {
   createdAt!: Date;
   snaps!: number;
   imageUrl!: string;
+  buttonText!: string;
 
   ngOnInit() {
     this.title = 'Archibald';
     this.description = "C'est un ami de tout petit !";
     this.createdAt = new Date();
     this.snaps = 6;
-    this.imageUrl = 'https://cdn.pixabay.com/photo/2022/06/08/15/21/artist-7250697_640.jpg'
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2022/06/08/15/21/artist-7250697_640.jpg';
+    this.buttonText = 'Oh snaps !';
+  }
+
+  onSnaps() {
+    if (this.buttonText === 'Oh snaps !') {
+      this.snaps++;
+      this.buttonText = 'Oops, unsnaps !';
+    } else {
+      this.snaps--;
+      this.buttonText = 'Oh snaps !'
+    }
   }
 
 }
